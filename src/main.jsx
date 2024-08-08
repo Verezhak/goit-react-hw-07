34def991bb41336ee3b47085273b4a4289aabc89
Verezhak <153289346+Verezhak@users.noreply.github.com>
2024-08-08 23:26:15 +0300

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,10 @@ import 'modern-normalize';
 import App from './App.jsx';
 import './index.css';
 import { Provider } from 'react-redux';
-import { persistor, store } from './redux/store.js';
-import { PersistGate } from 'redux-persist/integration/react';
+import { store } from './redux/store.js';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      <App />
-    </PersistGate>
-
+    <App />
   </Provider>
 )
